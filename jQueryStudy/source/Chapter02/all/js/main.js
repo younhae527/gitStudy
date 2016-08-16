9a1484f95d94b73b9bebd3e27b174876d987fc14
김윤혜 <younhae527@gmail.com>
2016-08-16 16:44:55 +0900

    add Folder

---
+++ jQueryStudy/source/Chapter02/all/js/main.js
@@ -0,0 +1,5 @@
+$(function(){
+    //$('#typo').css('color','#ebc000');
+    //$('#typo .inner').css('transform','rotate(10deg)');
+    $('.page-main > div:nth-child(1) .inner').css('opacity','0.5');
+})
