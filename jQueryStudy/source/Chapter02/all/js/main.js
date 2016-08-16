415397b516cffdb88da82ce0aa3a6ad72c0b58fb
김윤혜 <younhae527@gmail.com>
2016-08-16 17:49:49 +0900

    mode files

--- jQueryStudy/source/Chapter02/all/js/main.js
+++ jQueryStudy/source/Chapter02/all/js/main.js
@@ -1,5 +1,17 @@
 $(function(){
     //$('#typo').css('color','#ebc000');
     //$('#typo .inner').css('transform','rotate(10deg)');
-    $('.page-main > div:nth-child(1) .inner').css('opacity','0.5');
+    //$('.page-main > div:nth-child(1) .inner').css('opacity','0.5');
+    /*
+    $('#typo').css({
+        'font-size':'50px',
+        'background-color':'#ae5e9b',
+        'color':'#ebc000'
+    });
+    */
+    $('#typo').css({
+        fontSize:'70px',
+        backgroundColor:'#ae5e9b',
+        color:'#abd005'
+    });
 })
